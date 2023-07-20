2b2ef7a71679d11de349d38164410113a4760646
Abdurahman <abdurahman.elmawi06@gmail.com>
2023-07-20 04:02:22 +0300

    add some tools

--- userland/tools.js
+++ userland/tools.js
@@ -4,20 +4,36 @@ create_file("/bin/js", function() {
     }
 });
 create_file("/bin/ls", function() {
+    let files;
     this.main = function(args) {
         let fd = open("/dev/stdin", "a");
-        if(!args)
-            write(fd, readdir("."));
-        else write(fd, readdir(args[0]));
+        let list_directory = function(path) {
+            files = readdir(path)
+            for(let i = 0; i < files.length; i++)
+                write(fd, files[i] + " ");
+        }
+        if(args.length === 0)
+            list_directory(".");
+        else list_directory(args[0]);
+        write(fd, "\n");
+        close(fd);
         exit();
     }
 });
 create_file("/bin/echo", function() {
-    this.main = function(path) {
+    this.main = function(args) {
         let fd = open("/dev/stdin", "a");
-        if(!path)
-            write(fd, readdir("."));
-        else write(fd, readdir(path));
+        for(let i = 0; i < args.length; i++)
+            write(fd, args[i] + " ");
+        exit();
+    }
+});
+create_file("/bin/cat", function() {
+    this.main = function(args) {
+        let fd = open("/dev/stdin", "a");
+        if(args.length === 0) write(fd, "A directory must be specified");
+        else write(fd, read(open(args[0], "r")));
+        write(fd, "\n");
         exit();
     }
 });
